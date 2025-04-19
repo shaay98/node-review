6aca3433e8453e31d250ba7a0cf37bcf69966fc6
shaay98 <nrichardson567@icloud.com>
2025-04-19 02:02:39 +0000

    first commit using npm package lodash and filtering package json

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+import { addition, peopleShuffle } from "./src/utils.js";
+
+
+console.log(peopleShuffle());  
+console.log(addition(7, 3, 2)); 
+
---
+++ src/utils.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+export function peopleShuffle () {
+  return ['Alexis', 'Zack', 'Kobe', 'Ray'];
+};
+
+export function addition(num1,num2,num3) {
+  return[num1 + num2 + num3]
+};
+
+
+
+
+
